d85cd10fdd1367115d7026ce130d2eaca0e84a06
Wictor Girardi <wictorgirardi@Wictors-MacBook-Pro.local>
2025-01-10 01:12:24 -0300

    adding cors config

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 import routes from "./routes";
 import pokemonRoutes from "./routes/pokemonRoutes";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT!;
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use("/api", routes);

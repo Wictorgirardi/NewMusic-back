cc106a99f9e22ae5c7ee641944924fb7eb63d490
Wictor Girardi <wictorgirardi@Wictors-MacBook-Pro.local>
2025-01-09 23:36:05 -0300

    adding current page and total pages in pokemon endpoint

--- src/controllers/pokemonController.ts
+++ src/controllers/pokemonController.ts
@@ -7,15 +7,19 @@ export const listKantoPokemons: RequestHandler = async (
 ): Promise<void> => {
   try {
     const { page = 1, limit = 20, search = "" } = req.query;
-
+    const totalPokemons = 151;
     const pageNumber = parseInt(page as string, 10);
     const limitNumber = parseInt(limit as string, 10);
+    const totalPages = Math.ceil(totalPokemons / limitNumber);
     const offset = (pageNumber - 1) * limitNumber;
 
     const pokemons = await fetchPokemons(limitNumber, offset, search as string);
 
     res.status(200).json({
       message: "Sucesso ao buscar pokemons.",
+      totalPages,
+      totalPokemons,
+      currentPage: pageNumber,
       pokemons,
     });
   } catch (error: any) {
